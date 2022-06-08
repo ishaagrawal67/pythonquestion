afe0fdf5d56c123c65b9a38fcd7fdaae2643a4a0
ishaagrawal67 <isha.agrawal_cs20@gla.ac.in>
2022-06-09 00:13:09 +0530

    practice question

---
+++ program 9.py
@@ -0,0 +1,8 @@
+values = input("input some comma separated numbers :")
+list= values.split(",")
+tuple =tuple(list)
+print("list : ",list)
+print("tuple :",tuple)
+'''Write a program to find the factorial of a number.
+Write a program to find the sum of the digits of a number accepted from user
+ Write a program to check whether a number is prime or not'''

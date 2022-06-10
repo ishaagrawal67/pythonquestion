c6e12d74f919a28b9a35f48575364c2ec35e6bb9
ishaagrawal67 <isha.agrawal_cs20@gla.ac.in>
2022-06-10 18:13:00 +0530

    number is greator than 100

---
+++ number is greator than 100.py
@@ -0,0 +1,8 @@
+# write a program in which you entered a number is greator than 100 than break otherwise try again entered
+while(True):
+    n=int(input("enter the number"))
+    if(n>100):
+        print("congrates you entered a number is greator than 100")
+        break
+    else:
+        print("try again")

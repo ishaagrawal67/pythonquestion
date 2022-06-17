af03eb329fc38745cc4bff92c7ac3a4de64e9e0d
Isha Agarwal <79989964+ishaagrawal67@users.noreply.github.com>
2022-06-17 17:16:55 +0530

    Update exception.py

--- exception.py
+++ exception.py
@@ -1,12 +1,20 @@
-n=int(input("enter a number"))
-try:
-    if(n<0):
-        print('number is negative')
-except:
-    print("find the factoril of that number")
-    fct=1
-    for i in range(1,n+1):
-        fct=fct*i
-    print(fct)
+class myError(Exception):
+    pass
 
 
+def factorial():
+    n = int(input("enter a number"))
+    try:
+        if n<0:
+            raise myError('Negative Number')
+        else:
+            print(" factorial of the number")
+            fct = 1
+            for i in range(1, n + 1):
+                fct = fct * i
+            print(fct)
+    except myError as error:
+        print(error)
+
+
+factorial()

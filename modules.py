f8114de15780788a0c05f269e85baf285aded6b8
ishaagrawal67 <isha.agrawal_cs20@gla.ac.in>
2022-06-24 16:30:24 +0530

    modules

---
+++ modules.py
@@ -0,0 +1,14 @@
+import math
+print(math.sqrt(16))
+print(math.pow(2,3))
+print(math.pi)
+print(math.factorial(3))
+print(math.log10(100))
+print(math.log10(1000))
+print(math.floor(2.3))
+print(math.ceil(2.3))
+import calendar
+cal=calendar.month(2022,7)
+print(cal)
+print(calendar.isleap(2022))
+print(calendar.datetime())

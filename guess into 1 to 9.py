d1a3bf2e52b1112492063a87a45f6e7110ecac95
ishaagrawal67 <isha.agrawal_cs20@gla.ac.in>
2022-06-24 16:29:07 +0530

    guess into 1 to 9

---
+++ guess into 1 to 9.py
@@ -0,0 +1,9 @@
+print("number is guessing between into 1 to 9")
+while(1):
+    guess=int(input("Guess a number between 1 and 10 until you get it right :"))
+    if guess>1 and guess<10 :
+        print("well guessed!")
+        break
+
+
+
